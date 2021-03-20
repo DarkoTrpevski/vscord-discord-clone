c59b673153c0ed5944e0ab1a69470b38dfe8d3dd
Darko <darko_trpevski@yahoo.com>
2021-03-20 21:18:41 +0100

    Removed/Modified initial files

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+//Pages
+import LandingPage from './pages/LandingPage/LandingPage'
+import LoginPage from './pages/LoginPage/LoginPage'
+import RegisterPage from './pages/RegisterPage/RegisterPage'
 
 const App = () => {
   return (
     <div className="App">
-      <h1>React</h1>
+      <Router>
+        <Switch>
+
+          <Route exact path="/" render={(props) => (
+            <LandingPage {...props} />
+          )} />
+
+          <Route path={[`/login`, `/register`]}>
+            <Route path="/login" component={LoginPage} />
+            <Route path="/register" component={RegisterPage} />
+          </Route>
+
+          <Route path="/register" component={HomePage} />
+
+        </Switch>
+      </Router>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react"
+import store from './redux/store';
+import { theme } from './theme/theme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store = { store }>
+      <ChakraProvider theme = { theme } resetCSS = {true}>
+        <ColorModeScript />
+        <App />
+      </ChakraProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
